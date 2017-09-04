b2946e1a2bc0e27df8469467ff65455590f3c531
stephanecharron <stephanecharron@this.com>
2017-09-04 13:29:49 -0400

    refacto

--- app/directives/personalInformation/personalInformation.js
+++ app/directives/personalInformation/personalInformation.js
@@ -2,16 +2,14 @@
 
 let personalInformationDirective = angular.module('directives.personalInformation', [])
 
-personalInformationDirective.directive('personalInformation', [function(){
-  return {
-    restrict: 'EA',
-    templateUrl: 'directives/personalInformation/personalInformation.html',
-    scope: { data: '=' },
-    controller: function($scope) {
-      console.log($scope);
-      // logs $scope --> tu peux voir la propriété data dans la liste
-      console.log($scope.data)
-      // logs undefined --> wtf?
-    }
-  };
+personalInformationDirective.directive('personalInformation', ['$log', function ($log) {
+    return {
+        restrict: 'EA',
+        templateUrl: 'directives/personalInformation/personalInformation.html',
+
+        scope: {
+            personData: '='
+        }
+
+    };
 }]);
